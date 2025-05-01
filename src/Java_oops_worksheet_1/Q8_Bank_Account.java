a7daf9fa8fe6f8e416943429de416ffcac515577
Ankita Porwal <aporwal@venacorp.com>
2025-05-02 00:07:38 +0530

    final::accountNumber

--- src/Java_oops_worksheet_1/Q8_Bank_Account.java
+++ src/Java_oops_worksheet_1/Q8_Bank_Account.java
@@ -1,27 +1,34 @@
 package Java_oops_worksheet_1;
-class BankAccount1{
+
+class BankAccount1 {
     private String name;
     private double balance;
     private String type;
+    private final String accountNumber;  // made final
 
-    BankAccount1(String name,double balance,String type){
-        this.name=name;
-        this.balance=balance;
-        this.type=type;
+    BankAccount1(String name, double balance, String type, String accountNumber) {
+        this.name = name;
+        this.balance = balance;
+        this.type = type;
+        this.accountNumber = accountNumber; // initialized once
     }
 
-    public void updateBalance(double amount){
-        if(amount>=0) balance=amount;
+    public void updateBalance(double amount) {
+        if (amount >= 0) balance = amount;
         else System.out.println("This is not a valid balance to be updated..");
     }
 
-    public void getDetails(){
-        System.out.println("Name:"+name+"\tbalance:"+balance+"\ttype:"+type);
+    public void getDetails() {
+        System.out.println("Name: " + name +
+                "\tBalance: " + balance +
+                "\tType: " + type +
+                "\tAccount Number: " + accountNumber);
     }
 }
+
 public class Q8_Bank_Account {
-    public static void main(String ar[]){
-        BankAccount1 b=new BankAccount1("Ankita",50000.90,"savings");
+    public static void main(String ar[]) {
+        BankAccount1 b = new BankAccount1("Ankita", 50000.90, "savings", "ACC987654321");
         b.getDetails();
         b.updateBalance(89000.90);
         System.out.println("Updated details:");
